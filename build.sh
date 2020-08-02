97c396a96b0a6bcdae96bd537402d47e71d412de
Thomas J. Taylor <mail@thomasjtaylor.com>
2020-08-02 16:52:33 -0400

    Add favicon, dynamic sitemap

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p build
+chown -R $USER:$USER build
+cp *.html build
+cd build
+
+ROOT_URL=https://rent305.net
+SITEMAP=sitemap.xml
+DATE=`date -Is`
+
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' > $SITEMAP
+
+for f in *.html; do
+echo "  <url>
+    <loc>${ROOT_URL}/${f}</loc>
+    <lastmod>${DATE}</lastmod>
+  </url>" >> $SITEMAP
+done
+echo '</urlset>' >> $SITEMAP
